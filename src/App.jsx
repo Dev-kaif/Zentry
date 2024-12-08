2d756f3838ebffd00396fd5b482100b82218fcf7
Mohammad Kaif <161276501+Dev-kaif@users.noreply.github.com>
2024-12-08 19:57:30 +0530

    made the NavBar

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,11 @@
 import Hero from './Components/Hero'
 import About from './Components/About'
+import { Navbar } from './Components/Navbar'
 
 function App() {
   return (
     <div className='min-h-screen relative w-screen overflow-x-hidden'>
+      <Navbar/>
       <Hero/>
       <About/>
     </div>
