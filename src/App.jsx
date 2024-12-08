ec13dc9985f1d5459778e51fcb4b3836a4b5e6b2
Mohammad Kaif <161276501+Dev-kaif@users.noreply.github.com>
2024-12-08 16:38:36 +0530

    added about section to App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,12 @@
 import React from 'react'
 import Hero from './Components/Hero'
+import About from './Components/About'
 
 function App() {
   return (
     <div className='min-h-screen relative w-screen overflow-x-hidden'>
       <Hero/>
+      <About/>
     </div>
   )
 }
