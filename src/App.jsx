a02544d81eb74260920dce04ff0eb47485ce4a85
Mohammad Kaif <161276501+Dev-kaif@users.noreply.github.com>
2024-12-09 17:16:11 +0530

    comapleted the Story componenet

--- src/App.jsx
+++ src/App.jsx
@@ -2,6 +2,7 @@ import Hero from './Components/Hero'
 import About from './Components/About'
 import { Navbar } from './Components/Navbar'
 import Features from './Components/Features'
+import Story from './Components/Story'
 
 function App() {
   return (
@@ -10,6 +11,7 @@ function App() {
       <Hero/>
       <About/>
       <Features/>
+      <Story />
     </div>
   )
 }
---
+++ src/Components/Story.jsx
@@ -0,0 +1,88 @@
+import React from "react";
+import AnimatedTitile from "./AnimatedTitile";
+import Tilt from "react-parallax-tilt";
+import Button from "./Button";
+
+const Story = () => {
+  return (
+    <div id="story" className="min-h-dvh w-screen bg-black text-blue-50">
+      <div className="flex size-full flex-col items-center py-10 pb-24">
+        <div className="text-sm font-general uppercase  md:text-[10px]  ">
+          The Open IP Universe
+        </div>
+
+        <div className="relative size-full">
+          <AnimatedTitile
+            title={"The St<b>o</b>ry of <br /> a hidden real<b>m</b>e"}
+            sectionId="#story"
+            containerClass={
+              "mt-5 pointer-events-none mix-blend-difference relative z-10"
+            }
+          />
+
+          <div className="story-img-container">
+            <Tilt
+              transitionSpeed={500}
+              reset={true}
+              perspective={990}
+              className="story-img-mask"
+            >
+              <Tilt className="story-img-content">
+                <img
+                  src="/img/entrance.webp"
+                  className="object-contain"
+                  alt=""
+                />
+              </Tilt>
+            </Tilt>
+            
+            {/* For rounded Corners */}
+            <svg
+              className="invisible absolute size-0"
+              xmlns="http://www.w3.org/2000/svg"
+            >
+              <defs>
+                <filter id="flt_tag">
+                  <feGaussianBlur
+                    in="SourceGraphic"
+                    stdDeviation="8"
+                    result="blur"
+                  />
+                  <feColorMatrix
+                    in="blur"
+                    mode="matrix"
+                    values="1 0 0 0 0  0 1 0 0 0  0 0 1 0 0  0 0 0 19 -9"
+                    result="flt_tag"
+                  />
+                  <feComposite
+                    in="SourceGraphic"
+                    in2="flt_tag"
+                    operator="atop"
+                  />
+                </filter>
+              </defs>
+            </svg>
+          </div>
+        </div>
+
+        <div className="-mt-80 flex w-full justify-center md:-mt-64 md:me-44 md:justify-end">
+          <div className="flex h-full w-fit flex-col items-center md:items-start">
+            <p className="mt-3 max-w-sm text-center font-circular-web text-violet-50 md:text-start">
+              Where realms converge, lies Zentry and the boundless pillar.
+              Discover its secrets and shape your fate amidst infinite
+              opportunities.
+            </p>
+
+            <Button
+              id="realm-btn"
+              title="discover prologue"
+              containerClass="mt-5"
+            />
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Story;
