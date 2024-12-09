2512a0d0ae36446ea69921bc6e49cab65d32b6d4
Mohammad Kaif <161276501+Dev-kaif@users.noreply.github.com>
2024-12-09 19:20:33 +0530

    completed the contact section

--- src/App.jsx
+++ src/App.jsx
@@ -3,6 +3,8 @@ import About from './Components/About'
 import { Navbar } from './Components/Navbar'
 import Features from './Components/Features'
 import Story from './Components/Story'
+import Contact from './Components/Contact'
+import Footer from './Components/Footer'
 
 function App() {
   return (
@@ -12,6 +14,7 @@ function App() {
       <About/>
       <Features/>
       <Story />
+      <Contact/>
     </div>
   )
 }
