adc0bade027e586d8279c474e23c6bd305e3497f
Mohammad Kaif <161276501+Dev-kaif@users.noreply.github.com>
2024-12-10 10:27:28 +0530

    minor change in the loading fun

--- src/Components/Hero.jsx
+++ src/Components/Hero.jsx
@@ -44,7 +44,7 @@ const Hero = () => {
 
   // Effect to remove the loading screen once all videos are loaded
   useEffect(() => {
-    if (loaddedVideos === totalVideo - 3) {
+    if (loaddedVideos === 1) {
       setIsLoading(false); // Hide the loading screen
     }
   }, [loaddedVideos]); // Dependency: triggers when 'loaddedVideos' changes
