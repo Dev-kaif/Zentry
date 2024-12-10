7afcc0f4063bd88c6326197203d372f3620d457f
Mohammad Kaif <161276501+Dev-kaif@users.noreply.github.com>
2024-12-10 11:20:37 +0530

    fix

--- src/Components/Hero.jsx
+++ src/Components/Hero.jsx
@@ -104,7 +104,7 @@ const Hero = () => {
   return (
     <div className="relative h-dvh w-screen overflow-x-hidden">
       {/* Loading Screen */}
-      {/* {isLoading && (
+      {isLoading && (
         <div className="flex justify-center items-center absolute z-[100] h-dvh w-screen overflow-hidden bg-violet-500">
           <div className="three-body">
             <div className="three-body__dot"></div>
@@ -112,7 +112,7 @@ const Hero = () => {
             <div className="three-body__dot"></div>
           </div>
         </div>
-      )} */}
+      )}
 
       <div
         id="video-frame"
